4644bad0bcbb8f388b908e5cb2cd4faa52bf1f2a
deepjoy9 <deepjoysarkar9@gmail.com>
2024-03-22 01:07:35 +0530

    Refactor postRoutes

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const uploadMiddleware = require("../middlewares/uploadMiddleware");
-const { createPost, updatePost, getPosts, getPostById } = require("../controllers/postController");
+const {
+  createPost,
+  updatePost,
+  getPosts,
+  getPostById,
+} = require("../controllers/postController");
 
 router.post("/", uploadMiddleware.single("file"), createPost);
 router.put("/", uploadMiddleware.single("file"), updatePost);
