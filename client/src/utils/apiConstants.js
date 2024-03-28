ef52b7d01cafd492c56167cc412fa13db3d00103
deepjoy9 <deepjoysarkar9@gmail.com>
2024-03-28 22:13:26 +0530

    Added apiConstants.js for centralized API management

---
+++ client/src/utils/apiConstants.js
@@ -0,0 +1,7 @@
+export const REGISTER_API = "http://localhost:4000/api/auth/register";
+
+export const LOGIN_API = "http://localhost:4000/api/auth/login";
+
+export const LOGOUT_API = "http://localhost:4000/api/auth/logout";
+
+export const POSTS_API = "http://localhost:4000/api/posts";
