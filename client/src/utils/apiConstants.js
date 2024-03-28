4b54cf6e421fe2f518eb4ffe43a7c15ceef6047c
deepjoy9 <deepjoysarkar9@gmail.com>
2024-03-28 22:42:13 +0530

    Added User Profile Api

--- client/src/utils/apiConstants.js
+++ client/src/utils/apiConstants.js
@@ -4,4 +4,6 @@ export const LOGIN_API = "http://localhost:4000/api/auth/login";
 
 export const LOGOUT_API = "http://localhost:4000/api/auth/logout";
 
+export const USER_PROFILE_API = "http://localhost:4000/api/auth/profile";
+
 export const POSTS_API = "http://localhost:4000/api/posts";
