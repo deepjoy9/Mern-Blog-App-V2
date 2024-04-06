43540a9435a8e87a3e1d10c9f570f584caa3c66f
deepjoy9 <deepjoysarkar9@gmail.com>
2024-04-06 23:15:30 +0530

    Added comments to routes in post router & auth router

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, profile, logout } = require("../controllers/authController");
+const {
+  register,
+  login,
+  profile,
+  logout,
+} = require("../controllers/authController");
 
+// Route for user registration
 router.post("/register", register);
+
+// Route for user login
 router.post("/login", login);
+
+// Route for fetching user profile
 router.get("/profile", profile);
+
+// Route for user logout
 router.post("/logout", logout);
 
 module.exports = router;
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -10,11 +10,22 @@ const {
   deletePost,
 } = require("../controllers/postController");
 
+// Route for creating a new post
 router.post("/", uploadMiddleware.single("file"), createPost);
+
+// Route for updating an existing post
 router.put("/", uploadMiddleware.single("file"), updatePost);
+
+// Route for getting all posts
 router.get("/", getPosts);
+
+// Route for getting a specific post by ID
 router.get("/:id", getPostById);
+
+// Route for getting posts of a specific user
 router.get("/myposts/:id", getMyPosts);
+
+// Route for deleting a post by ID
 router.delete("/:id", deletePost);
 
 module.exports = router;
