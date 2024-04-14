31923b1af967118c149e616588480df6f1a5f73a
deepjoy9 <deepjoysarkar9@gmail.com>
2024-04-15 00:57:29 +0530

    Refactor auth controller functions for improved error handling

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,6 @@ const User = require("../models/User.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const secret = process.env.SECRET_KEY;
-const mongoose = require("mongoose");
 
 exports.register = async (req, res) => {
   const { username, password } = req.body;
@@ -13,57 +12,78 @@ exports.register = async (req, res) => {
       password: bcrypt.hashSync(password, salt),
     });
     res.json(userDoc);
-  } catch (e) {
-    res.status(400).json(e);
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(400).json({ error: "Registration failed. Please try again." });
   }
 };
 
 exports.login = async (req, res) => {
-  const { username, password } = req.body;
-  const userDoc = await User.findOne({ username });
-  if (!userDoc) {
-    return res.status(400).json({ message: "User not found" });
-  }
-  const passOk = bcrypt.compareSync(password, userDoc.password);
+  try {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username });
+    if (!userDoc) {
+      return res.status(400).json({ message: "User not found" });
+    }
+    const passOk = bcrypt.compareSync(password, userDoc.password);
 
-  if (passOk) {
-    //Logged in
-    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-      if (err) throw err;
-      const expiryDate = new Date(Date.now() + 3600000); // 1 hour
-      res
-        .cookie("token", token, {
-          sameSite: "none",
-          secure: true,
-          expires: expiryDate,
-        })
-        .json({
-          id: userDoc._id,
-          username,
-        });
-    });
-  } else {
-    res.status(400).json("Wrong Credentials");
+    if (passOk) {
+      jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+        if (err) {
+          console.error("Error generating JWT:", err);
+          return res.status(500).json({ error: "Internal Server Error" });
+        }
+        const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+        res
+          .cookie("token", token, {
+            sameSite: "none",
+            secure: true,
+            expires: expiryDate,
+          })
+          .json({
+            id: userDoc._id,
+            username,
+          });
+      });
+    } else {
+      res.status(400).json({ error: "Wrong credentials" });
+    }
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 exports.profile = (req, res) => {
-  const { token } = req.cookies;
-  if (!token) {
-    return res.status(401).json({ error: "Token is not valid" });
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).json({ error: "Token is not valid" });
+    }
+    jwt.verify(token, secret, {}, (err, info) => {
+      if (err) {
+        console.error("Error verifying token:", err);
+        return res.status(401).json({ error: "Token is not valid" });
+      }
+      res.json(info);
+    });
+  } catch (error) {
+    console.error("Error fetching user profile:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-  jwt.verify(token, secret, {}, (err, info) => {
-    if (err) throw err;
-    res.json(info);
-  });
 };
 
 exports.logout = (req, res) => {
-  res
-    .clearCookie("token", {
-      sameSite: "none",
-      secure: true,
-    })
-    .status(200)
-    .json("Logout success!");
+  try {
+    res
+      .clearCookie("token", {
+        sameSite: "none",
+        secure: true,
+      })
+      .status(200)
+      .json("Logout success!");
+  } catch (error) {
+    console.error("Error logging out:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
