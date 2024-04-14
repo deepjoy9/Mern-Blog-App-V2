0f4bd791dfba778bbc5a5a2c79795f774da45187
deepjoy9 <deepjoysarkar9@gmail.com>
2024-04-14 21:11:18 +0530

    Refactor PostSchema for stricter field validations

--- server/models/Post.model.js
+++ server/models/Post.model.js
@@ -3,11 +3,29 @@ const { Schema, model } = mongoose;
 
 const PostSchema = new Schema(
   {
-    title: String,
-    summary: String,
-    content: String,
-    cover: String,
-    author:{type:Schema.Types.ObjectId, ref:'User'},
+    title: {
+      type: String,
+      required: true,
+      maxlength: 100,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      maxlength: 500,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   {
     timestamps: true,
