11313299178bc0a8c663f9035fbd5ee66aa5989e
deepjoy9 <deepjoysarkar9@gmail.com>
2024-05-06 21:06:08 +0530

    Refactor data fetching in IndexPage component
    - Refactored data fetching to use async/await for cleaner asynchronous code.
    - Added error handling to catch any errors during data fetching.
    - Improved code readability and conciseness by using a ternary operator to handle loading, error, and successful data fetching conditions.
    - Utilized useMemo hook to memoize the posts data, ensuring recalculation only when its dependencies change.

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Post from "../components/Post";
 import { POSTS_API } from "../utils/apiConstants";
 import ShimmerList from "../components/ShimmerUI";
@@ -6,22 +6,38 @@ import ShimmerList from "../components/ShimmerUI";
 const IndexPage = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    const response = fetch(POSTS_API).then((response) => {
-      response.json().then((posts) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(POSTS_API);
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const posts = await response.json();
         setPosts(posts);
+      } catch (error) {
+        setError(error.message);
+      } finally {
         setLoading(false);
-      });
-    });
+      }
+    };
+
+    fetchData();
   }, []);
+
+  const memoizedPosts = useMemo(() => posts, [posts]);
 
   return (
     <>
       {loading ? (
-        <ShimmerList /> // Render shimmer UI if loading, else render posts
+        <ShimmerList />
+      ) : error ? (
+        <div>Error: {error}</div>
       ) : (
-        posts.length > 0 &&
-        posts.map((post) => <Post key={post._id} {...post} />)
+        memoizedPosts.length > 0 &&
+        memoizedPosts.map((post) => <Post key={post._id} {...post} />)
       )}
     </>
   );
